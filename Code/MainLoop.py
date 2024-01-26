655a822d92cfac668b36a4ebfc4b56339f440c1e
PineappleW3 <156610890+PineappleW3@users.noreply.github.com>
2024-01-26 09:49:35 +0000

    finishe main loop

--- Code/MainLoop.py
+++ Code/MainLoop.py
@@ -5,10 +5,11 @@ from EditTeam import *
 
 print("Welcome to the Pokemon battle tower helper")
 teamloaded = False
+recentlysaved = True
 
 while True:
     try:
-        choice = int(input("What would you like to do?\n1)Create a new team\n2)Load an existing team\n3)Edit the currently loaded team\n4)Save the currently loaded team\n5)Close the program\nPlease input the number:"))
+        choice = int(input("\nWhat would you like to do?\n1)Create a new team\n2)Load an existing team\n3)Edit the currently loaded team\n4)Save the currently loaded team\n5)Close the program\nPlease input the number:"))
     except:
         choice = "Beans"
 
@@ -18,9 +19,11 @@ while True:
             if choice2.upper() == "Y":
                 team = CreateTeam()
                 teamloaded = True
+                recentlysaved = True
         else:
             team = CreateTeam()
             teamloaded = True
+            recentlysaved = True
 
     elif choice == 2:
         if teamloaded == True:
@@ -28,27 +31,33 @@ while True:
             if choice2.upper() == "Y":
                 team = LoadTeam()
                 teamloaded = True
+                recentlysaved = True
         else:
             team = LoadTeam()
             teamloaded = True
+            recentlysaved = True
 
     elif choice == 3:
         if teamloaded == True:
             ChangeTeam(team)
+            recentlysaved = False
         else:
             print("You need to load a team first")
     
     elif choice == 4:
         if teamloaded == True:
             SaveTeam(team)
+            recentlysaved = True
         else:
             print("You need to load a team first")
     
     elif choice == 5:
-        if teamloaded == True:
+        if recentlysaved == False:
             choice2 = input("Would you like to save first? (Y/N)")
             if choice2.upper() == "Y":
                 SaveTeam(team)
+                break
+            else:
                 break
         else:
             break
