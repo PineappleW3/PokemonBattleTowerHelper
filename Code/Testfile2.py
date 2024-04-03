0cc7d474ea1764610eda6dee55f226fbfd25e567
Pineapple3 <casperomoorhouse@icloud.com>
2024-04-03 23:13:36 +0100

    more testing

--- Code/Testfile2.py
+++ Code/Testfile2.py
@@ -1,8 +1,25 @@
-from CreateTeam import *
-from LoadTeam import *
-from SaveTeam import *
-from EditTeam import *
+from BonusInfo import *
+from CalculateDamage import *
+from ChangeStatus import *
+from CreateOpponent import *
+from CullOpponents import *
+from GetItem import *
 from GetLevel import *
+from GetMoves import *
+from ChooseSpecies import *
+from GetAbility import *
+from GetHP import *
+import sys
 
-number = GetLevel()
-print(number)
+import math
+import sqlite3
+
+#sets the directory for the database file
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'Pokemon.db')
+
+connection = sqlite3.connect(filename)
+cursor = connection.cursor()
+
+ChooseSpecies()
