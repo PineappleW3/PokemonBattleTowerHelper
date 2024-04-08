faddaf71126fb565ce5a3dac4daa6fca5891c33d
PineappleW3 <156610890+PineappleW3@users.noreply.github.com>
2024-04-08 12:38:05 +0000

    not sure what i changed tbh

--- Code/DisplayOpponents.py
+++ Code/DisplayOpponents.py
@@ -1,6 +1,7 @@
 def DisplayOpponents(opponents,expectedmoves):
     print("\n\n")
     counter = len(opponents)
+    #loops for every potential opponent
     for i in range(0,counter):
         opponent = opponents[i]
         moves = expectedmoves[i]
@@ -8,13 +9,17 @@ def DisplayOpponents(opponents,expectedmoves):
         print (name + " " + str(i+1))
         print("Held Item: " + opponent.getitem())
         print("Ability: " + opponent.getability())
+        #shows every move
         moves2 = opponent.getmoves()
         for j in range(0,4):
             move2 = moves2[j]
             print("Move " + str(j+1) + ": " + move2)
+            #strongest move from previous function
         print("Highest damage move: " + moves[0][0] + ", dealing " + str(moves[0][1]) + " damage")
         status = moves[1]
         print("Status moves:")
         for j in status:
             print (j)
         print("\n\n")
+
+        
